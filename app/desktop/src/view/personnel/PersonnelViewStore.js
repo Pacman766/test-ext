809a0c51abe99719e985321e177c9fcca72c4930
pacman766 <3334976@gmail.com>
2022-04-07 15:53:23 +0300

    first commit

---
+++ app/desktop/src/view/personnel/PersonnelViewStore.js
@@ -0,0 +1,47 @@
+Ext.define('ModernApp.view.personnel.PersonnelViewStore', {
+  extend: 'Ext.data.Store',
+  alias: 'store.personnelviewstore',
+  fields: ['name', 'email', 'phone', 'dept'],
+  groupField: 'dept',
+  data: {
+    items: [
+      {
+        name: 'Jean Luc',
+        email: 'jeanluc.picard@enterprise.com',
+        phone: '555-111-1111',
+        dept: 'bridge',
+      },
+      {
+        name: 'Ivan Ivanov',
+        email: '4444@gmail.com',
+        phone: '888-343-4340',
+        dept: 'bridge',
+      },
+      {
+        name: 'ModernWorf',
+        email: 'worf.moghsson@enterprise.com',
+        phone: '555-222-2222',
+        dept: 'engine',
+      },
+      {
+        name: 'Deanna',
+        email: 'deanna.troi@enterprise.com',
+        phone: '555-333-3333',
+        dept: 'bridge',
+      },
+      {
+        name: 'Data',
+        email: 'mr.data@enterprise.com',
+        phone: '555-444-4444',
+        dept: 'bridge',
+      },
+    ],
+  },
+  proxy: {
+    type: 'memory',
+    reader: {
+      type: 'json',
+      rootProperty: 'items',
+    },
+  },
+});
